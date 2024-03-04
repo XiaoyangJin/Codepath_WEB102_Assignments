a4847e16bc40aa63d7861091258eef3c1152cfd8
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-03-04 15:36:06 -0500

    Add answer and redesign the page basic font

--- flashcards/src/components/Main.jsx
+++ flashcards/src/components/Main.jsx
@@ -3,7 +3,7 @@ import Card from "./Card";
 
 const Main = () => {
     return (
-        <Card question="What is Bubble Sort" />
+        <Card question="What is Bubble Sort" answer="A comparison-based sorting algorithm where each pair of adjacent elements is compared and swapped if they are in the wrong order, effectively 'bubbling' the highest or lowest element to its correct position in each iteration." />
     )
 }
 
