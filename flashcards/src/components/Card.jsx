78b882747a7a61be7b180c20537c7b5f24172430
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-03-04 15:45:00 -0500

    Add hover to flip effect

--- flashcards/src/components/Card.jsx
+++ flashcards/src/components/Card.jsx
@@ -4,14 +4,12 @@ const Card = (props) => {
     return (
         <div className="card__grid__container">
             <div className="card">
-                <div className="card__left"></div>
                 <div className="card__front">
                     <h3 className="card__question">{props.question}</h3>
                 </div>
                 <div className="card__back">
                     <h3 className="card__answer">{props.answer}</h3>
                 </div>
-                <div className="card__right"></div>
             </div>
         </div>
     )
