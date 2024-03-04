95ed9d113d6771b9cc2ffe159faef4ad089f2e8a
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-03-04 09:28:20 -0500

    Clean up useless code and generate a header with logo

---
+++ flashcards/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div className="Header">
+            <div className="header">
+                <div className="header__top">
+                    <img src="src/images/leetcode_logo.png" alt="the leetcode logo"></img>
+                    <h1 className="header__title">Leetcode Quiz</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
