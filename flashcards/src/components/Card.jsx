79637a9e04c0a85a0e886fbbd8c119e2267ef68f
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-03-04 15:28:02 -0500

    Set responsive card size

--- flashcards/src/components/Card.jsx
+++ flashcards/src/components/Card.jsx
@@ -2,12 +2,16 @@ import React from "react";
 
 const Card = (props) => {
     return (
-        <div className="card">
-            <div className="card__front">
-                <h3 className="card__question">{props.question}</h3>
-            </div>
-            <div className="card__back">
-                <h3 className="card__answer">{props.answer}</h3>
+        <div className="card__grid__container">
+            <div className="card">
+                <div className="card__left"></div>
+                <div className="card__front">
+                    <h3 className="card__question">{props.question}</h3>
+                </div>
+                <div className="card__back">
+                    <h3 className="card__answer">{props.answer}</h3>
+                </div>
+                <div className="card__right"></div>
             </div>
         </div>
     )
