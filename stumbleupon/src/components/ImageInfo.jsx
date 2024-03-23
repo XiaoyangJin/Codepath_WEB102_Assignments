80ca9e833800c19fb0060dae150c827207008dba
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-03-22 22:50:56 -0400

    Extract image detail into component

---
+++ stumbleupon/src/components/ImageInfo.jsx
@@ -0,0 +1,16 @@
+const ImageInfo = () => {
+    return (
+        <div className="image__info">
+            <div className="image__name"></div>
+            <div className="image__detail">
+                <div className="image__detail-person"></div>
+                <div className="image__detail-date"></div>
+                <div className="image__detail-culture"></div>
+                <div className="image__detail-color"></div>
+            </div>
+            <img src=""></img>
+        </div>
+    )
+}
+
+export default ImageInfo;
--- stumbleupon/src/components/Information.jsx
+++ stumbleupon/src/components/Information.jsx
@@ -1,9 +1,12 @@
+import ImageInfo from "./ImageInfo";
+
 const Information = () => {
     return (
         <div className="information">
             <h1 className="title">Welcome to Harvard Gallery!</h1>
             <div className="subtitle">Discover some amazing master pieces here!</div>
             <div className="emoji">🎨 🖼️ 📷 🗿 🗺️ 🧑‍🎨 🌌 🧩</div>
+            <ImageInfo />
             <button className="discover__btn">🔄 Discover</button>
         </div>
     )
