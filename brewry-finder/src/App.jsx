5a9c4691718c7495c110bb0af27b2e17bd64e290
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-03-29 22:13:17 -0400

    Remove default data and get lists from api

--- brewry-finder/src/App.jsx
+++ brewry-finder/src/App.jsx
@@ -1,34 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [breweries, setBreweries] = useState([]);
+
+  const [filteredResults, setFilteredResults] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+  useEffect(() => {
+    const fetchAllBreweryData = async () => {
+      try {
+        const response = await fetch("https://api.openbrewerydb.org/v1/breweries");
+        const data = await response.json();
+        setBreweries(data); // Assuming the API returns an array of breweries
+      } catch (error) {
+        console.error("Failed to fetch breweries:", error);
+      }
+    };
+
+    fetchAllBreweryData();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="whole-page">
+      <h1>Find Brewery</h1>
+      <ul>
+        {breweries.map((brewery) => (
+          <li key={brewery.id}>
+            {brewery.name} - {brewery.brewery_type} - {brewery.city}, {brewery.state}
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
