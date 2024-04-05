fc8350bf500b0a760f3b23e3532ac03294ca859a
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-04-05 16:40:38 -0400

    Update list into links

--- brewry-finder/src/components/BreweryList.jsx
+++ brewry-finder/src/components/BreweryList.jsx
@@ -1,9 +1,13 @@
+import { Link } from "react-router-dom";
+
 function BreweryList({ breweries }) {
     return (
         <ul className='list'>
-            {breweries.map(brewery => (
+            {breweries.map((brewery) => (
                 <li key={brewery.id} className='list__item'>
-                    {brewery.name} - {brewery.brewery_type} - {brewery.city}, {brewery.state}
+                    <Link to={`/brewery/${brewery.id}`}>
+                        {brewery.name} - {brewery.brewery_type} - {brewery.city}, {brewery.state}
+                    </Link>
                 </li>
             ))}
         </ul>
