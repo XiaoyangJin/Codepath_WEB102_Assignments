14514a737f188ffda22f6debbdb7b9780c6b84b9
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-04-12 23:23:42 -0400

    Add routes to other components

--- crewmates/src/main.jsx
+++ crewmates/src/main.jsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import CreateCrew from './components/CreateCrew.jsx';
+import Gallery from './components/Gallery.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route index={true} path="/" element={<App />} />
+        <Route index={true} path="/create" element={<CreateCrew />} />
+        <Route index={true} path="/gallery" element={<Gallery />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
