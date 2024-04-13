b1c0726b5714cdffd704aae4321a2d2168b36995
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-04-12 23:34:52 -0400

    Add links in navbar

--- crewmates/src/components/Navbar.jsx
+++ crewmates/src/components/Navbar.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Navbar = () => {
+function Navbar() {
     return (
-        <div>Navbar</div>
-    )
+        <nav>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/create">Create Crew</Link></li>
+                <li><Link to="/gallery">Gallery</Link></li>
+            </ul>
+        </nav>
+    );
 }
 
-export default Navbar
+export default Navbar;
