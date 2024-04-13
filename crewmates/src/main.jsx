a6ea756463c72d62b706796c169b46b8cac2cbf7
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-04-12 23:36:00 -0400

    Make Navbar stay forever

--- crewmates/src/main.jsx
+++ crewmates/src/main.jsx
@@ -5,10 +5,12 @@ import './index.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CreateCrew from './components/CreateCrew.jsx';
 import Gallery from './components/Gallery.jsx';
+import Navbar from './components/Navbar.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route index={true} path="/" element={<App />} />
         <Route index={true} path="/create" element={<CreateCrew />} />
