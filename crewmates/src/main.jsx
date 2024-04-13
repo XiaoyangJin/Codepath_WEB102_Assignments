2352914bd4ccadf5d9c788f5d0558c913b4410d9
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-04-13 14:33:35 -0400

    Change the root page to be home component

--- crewmates/src/main.jsx
+++ crewmates/src/main.jsx
@@ -6,13 +6,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CreateCrew from './components/CreateCrew.jsx';
 import Gallery from './components/Gallery.jsx';
 import Navbar from './components/Navbar.jsx';
+import Home from './components/Home.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route index={true} path="/" element={<App />} />
+        <Route index={true} path="/" element={<Home />} />
         <Route index={true} path="/create" element={<CreateCrew />} />
         <Route index={true} path="/gallery" element={<Gallery />} />
       </Routes>
