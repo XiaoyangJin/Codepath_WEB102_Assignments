d9949c4353ba480276baf61e33b9a7e869e95925
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-04-13 14:32:57 -0400

    Add content in home page

--- crewmates/src/components/Home.jsx
+++ crewmates/src/components/Home.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React from 'react';
+import '../css/Home.css';
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <div className='home__content'>
+            <h1 className='home__title'>Welcome to Crewmate Creator!</h1>
+            <div className='home__intro'>Here is where you can create a set of crewmates</div>
+            <img src='src/images/hero.jpg' alt='two yellow cartoon characters painting a wall' />
+        </div>
     )
 }
 
