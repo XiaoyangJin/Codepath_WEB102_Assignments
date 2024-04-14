f35d7827f258b25db43809a026fb1b4e3bfd354b
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-04-13 22:33:48 -0400

    Fetch data from Supabase

--- crewmates/src/components/Gallery.jsx
+++ crewmates/src/components/Gallery.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import { useEffect } from 'react';
+import { useState } from 'react';
 
-const Gallery = () => {
+import '../css/Gallery.css';
+import { supabase } from '../client';
+import Card from './Card';
+
+const Gallery = (props) => {
+
+    const [crews, setCrews] = useState([]);
+
+    useEffect(() => {
+        const fetchCrews = async () => {
+            const { data } = await supabase
+                .from('Detail')
+                .select();
+
+            setCrews(data);
+        }
+        fetchCrews();
+    }, [props]);
+
     return (
-        <div>Gallery</div>
+        <div className='gallery__content'>
+            <img src='src/images/crew.jpg' alt='a yellow character standing on desk' />
+            {
+                crews && crews.length > 0 ?
+                    crews.map((crew) => <Card id={crew.id} name={crew.name} speed={crew.speed} />) :
+                    <h2 className='gallery__empty'>{'No Crewmate Yet!'}</h2>
+            }
+        </div>
     )
 }
 
