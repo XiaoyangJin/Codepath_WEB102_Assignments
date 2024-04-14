bfa95fd1f90735f5c8a1a8fb71b236ccb8bf7d76
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-04-14 10:12:19 -0400

    Add key in list

--- crewmates/src/components/Gallery.jsx
+++ crewmates/src/components/Gallery.jsx
@@ -26,7 +26,7 @@ const Gallery = (props) => {
             <img src='src/images/crew.jpg' alt='a yellow character standing on desk' />
             {
                 crews && crews.length > 0 ?
-                    crews.map((crew) => <Card id={crew.id} name={crew.name} speed={crew.speed} />) :
+                    crews.map((crew) => <Card key={crew.id} id={crew.id} name={crew.name} speed={crew.speed} />) :
                     <h2 className='gallery__empty'>{'No Crewmate Yet!'}</h2>
             }
         </div>
