3d737807d94c286ee653f104ad200afdeabc3349
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-04-14 10:36:40 -0400

    Add route to edit page

--- crewmates/src/main.jsx
+++ crewmates/src/main.jsx
@@ -7,6 +7,7 @@ import CreateCrew from './components/CreateCrew.jsx';
 import Gallery from './components/Gallery.jsx';
 import Navbar from './components/Navbar.jsx';
 import Home from './components/Home.jsx';
+import EditCrew from './components/EditCrew.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -16,6 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route index={true} path="/" element={<Home />} />
         <Route index={true} path="/create" element={<CreateCrew />} />
         <Route index={true} path="/gallery" element={<Gallery />} />
+        <Route index={true} path="/gallery/edit/:id" element={<EditCrew />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
