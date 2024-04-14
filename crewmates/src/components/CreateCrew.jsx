63cbd30359b29439ebbae38d9be871dd0b45b434
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-04-13 21:00:30 -0400

    Add create form and some layout setup

--- crewmates/src/components/CreateCrew.jsx
+++ crewmates/src/components/CreateCrew.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../css/CreateCrew.css';
 
 const CreateCrew = () => {
+
+    const [crew, setCrew] = useState({ crewname: '', speed: 0 });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setCrew((prev) => {
+            return {
+                ...prev,
+                [name]: value,
+            }
+        })
+    };
+
     return (
         <div className='create__content'>
             <h1 className='title'>Create a New Crewmate</h1>
             <img src='src/images/crewmates.jpg' alt='a set of yellow cartoon characters stand in a line' />
+            <form className='create__form'>
+                <lable className='create__form-lable' for='crewname'>Crew Name: </lable><br />
+                <input className='create__form-input' type='text' id='crewname' name='crewname' onChange={handleChange} /><br />
+
+                <lable className='create__form-lable' for='speed'>Speed(mph): </lable><br />
+                <input className='create__form-input' type='text' id='speed' name='speed' onChange={handleChange} /><br />
+
+                <input className='create__form-button' type="submit" value="Submit" />
+            </form>
         </div>
     )
 }
