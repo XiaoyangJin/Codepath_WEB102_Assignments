043581846aee68e68cc3da4f7eea8461f1e2de0b
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-04-14 15:00:02 -0400

    Change for to htmlFor

--- crewmates/src/components/CreateCrew.jsx
+++ crewmates/src/components/CreateCrew.jsx
@@ -30,10 +30,10 @@ const CreateCrew = () => {
             <h1 className='title'>Create a New Crewmate</h1>
             <img src='src/images/crewmates.jpg' alt='a set of yellow cartoon characters stand in a line' />
             <form className='create__form'>
-                <lable className='create__form-lable' for='crewname'>Crew Name: </lable><br />
+                <lable className='create__form-lable' htmlFor='crewname'>Crew Name: </lable><br />
                 <input className='create__form-input' type='text' id='crewname' name='crewname' onChange={handleChange} /><br />
 
-                <lable className='create__form-lable' for='speed'>Speed(mph): </lable><br />
+                <lable className='create__form-lable' htmlFor='speed'>Speed(mph): </lable><br />
                 <input className='create__form-input' type='text' id='speed' name='speed' onChange={handleChange} /><br />
 
                 <input className='create__form-button' type="submit" value="Submit" onClick={createCrew} />
