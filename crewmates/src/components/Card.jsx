8c8a7e43a94846012de55f471b9c4e28a63e874a
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-04-14 10:16:36 -0400

    Add edit link to edit crew info

--- crewmates/src/components/Card.jsx
+++ crewmates/src/components/Card.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import '../css/Card.css';
+import { Link } from 'react-router-dom';
+
 
 const Card = (props) => {
     return (
         <div className="Card">
-            {/* <Link to={'edit/'+ props.id}><img className="moreButton" alt="edit button" src={more} /></Link> */}
+            <Link to={'edit/' + props.id}>Edit</Link>
             <h2 className="name">{props.name}</h2>
             <h3 className="speed">{"it's speed is " + props.speed}</h3>
         </div>
