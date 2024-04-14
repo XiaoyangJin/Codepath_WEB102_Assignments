cb9fc0a60be629a74dabc979da664da1ca5048f8
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-04-14 15:09:53 -0400

    Add card layout

--- crewmates/src/components/Card.jsx
+++ crewmates/src/components/Card.jsx
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom';
 const Card = (props) => {
     return (
         <div className="Card">
-            <Link to={'edit/' + props.id}>Edit</Link>
+            <Link className='edit__button' to={'edit/' + props.id}>Edit</Link>
             <h2 className="name">{props.name}</h2>
             <h3 className="speed">{"it's speed is " + props.speed}</h3>
         </div>
